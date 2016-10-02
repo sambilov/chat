696eeb3f908a28dd671b17394883207d76ef9388
sambilov <sambilov@gmail.com>
2016-10-02 18:53:25 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    devtool: 'cheap-module-eval-source-map',
+    entry: [
+        'webpack-hot-middleware/client',
+        'babel-polyfill',
+        './client/index'
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/static/'
+    },
+    plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ],
+    module: {
+        preLoaders: [
+            {
+                test: /\.js$/,
+                loaders: ['eslint'],
+                include: [
+                    path.resolve(__dirname, "client"),
+                ],
+            }
+        ],
+        loaders: [
+            {
+                loaders: [ 'babel-loader' ],
+                include: [
+                    path.resolve(__dirname, "client"),
+                ],
+                test: /\.js$/,
+                plugins: [ 'transform-runtime' ],
+            }
+        ]
+    }
+};
